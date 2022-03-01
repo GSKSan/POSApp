8faf9d5d2b3a8fd60740b266de06fba1274ae2d4
GSKSan <sksanthoshg94@gmail.com>
2022-03-01 23:22:57 +1300

    Add log class

---
+++ POSApp/Log/ILogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POSApp.Log
+{
+    public interface ILogger
+    {
+        void LogMessage(string message);
+    }
+}
